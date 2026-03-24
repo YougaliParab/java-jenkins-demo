f06a0d96dcd47204c40b8d65ef941a83ad5bce86
YougaliParab <156080362+YougaliParab@users.noreply.github.com>
2026-03-24 09:37:17 +0530

    Fix formatting and syntax in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	 public static void main(String[] args) {
 	        System.out.println("Hello, Jenkins + GitHub!");
 		    System.out.println("Heyyyyy");
-		 System.out.println("Auto build triggered!")
+		 System.out.println("Auto build triggered!");
 		 System.out.println("Auto build second time triggered!");
 		 System.out.println("Auto build third time triggered!");
 		  System.out.println("Auto build third time triggered!");
